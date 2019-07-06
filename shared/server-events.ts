f3429812d7a864214c37304ada1a004f15ef0b7d
daart <artemko51@gmail.com>
2019-07-06 21:34:33 +0300

    added msg player left the game

--- shared/server-events.ts
+++ shared/server-events.ts
@@ -2,3 +2,4 @@ export const JOINED_GAME: string = 'player-joined-game';
 export const GAMES_DATA: string = 'games-data';
 export const PLAYERS_DATA: string = 'players-data';
 export const GAME_INFO: string = 'game-info';
+export const PLAYER_LEFT: string = 'player-left';
