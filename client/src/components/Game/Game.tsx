a715dee1837329946f1348770b203df16251ebfb
daart <artemko51@gmail.com>
2019-07-06 21:03:06 +0300

    added gameInfo render on Game component

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -2,17 +2,30 @@ import * as React from 'react';
 import { socket } from '../../socket';
 
 import { LEAVE_GAME } from '../../../../shared/client-events';
+import { GAME_INFO } from '../../../../shared/server-events';
 import { useAppState } from '../../hooks/useAppState';
 
 export const Game = () => {
   const { appState, setAppState } = useAppState();
-  
+  const [gameInfo, setGameInfo] = React.useState(null);
+
+  React.useEffect(() => {
+    const onGameInfo = gameData => {
+      setGameInfo(gameData);
+    };
+    socket.on(GAME_INFO, onGameInfo);
+
+    return () => {
+      socket.off(GAME_INFO, onGameInfo);
+    };
+  }, []);
+
   const onLeaveHandler = () => {
     socket.emit(LEAVE_GAME, appState.gameId);
     setAppState({
       ...appState,
-      gameId: null
-    })
+      gameId: null,
+    });
   };
 
   if (!appState.gameId) {
@@ -21,6 +34,7 @@ export const Game = () => {
 
   return (
     <>
+      <pre>{JSON.stringify(gameInfo, null, 2)}</pre>
       <div>game #{appState.gameId}</div>
       <button onClick={onLeaveHandler}>Leave stupid game</button>
     </>
