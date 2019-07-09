b009eb6a21c7f09f09dc2afe260010bfda88f27c
Denys Izhetskyi <mail.bigjey@gmail.com>
2019-07-10 00:22:09 +0200

    example of countdown and circle progress

--- client/src/playground.tsx
+++ client/src/playground.tsx
@@ -7,6 +7,7 @@ import { Button } from './components/UI';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { CircleProgress } from './components/UI/CircleProgress';
 
 const Example = ({ name = null, children }) => (
   <div style={{ margin: '10px 0' }}>
@@ -15,9 +16,103 @@ const Example = ({ name = null, children }) => (
   </div>
 );
 
+const Countdown = ({
+  start = 10,
+  end = 0,
+  step = 1,
+  interval = 1000,
+  render,
+}) => {
+  const [value, setValue] = React.useState(start);
+  const v = React.useRef(start);
+
+  React.useEffect(() => {
+    const s = Math.sign(end - start) * step;
+
+    const id = window.setInterval(() => {
+      v.current += s;
+
+      if (start > end && v.current <= end) {
+        window.clearInterval(id);
+      } else if (start < end && v.current >= end) {
+        window.clearInterval(id);
+      }
+
+      setValue(v.current);
+    }, interval);
+
+    return () => {
+      window.clearInterval(id);
+    };
+  }, []);
+
+  return render(value);
+};
+
+Countdown.defaultProps = {
+  render: (value: any) => null,
+};
+
 render(
   <Page>
+    <Example>
+      <Countdown
+        start={0}
+        end={10}
+        render={value => (
+          <div style={{ position: 'relative', display: 'inline-block' }}>
+            <CircleProgress percentage={(value / 10) * 100} />
+            <div
+              style={{
+                position: 'absolute',
+                display: 'flex',
+                width: '100%',
+                height: '100%',
+                top: 0,
+                left: 0,
+                textAlign: 'center',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                fontSize: 40,
+                fontWeight: 'bold',
+              }}
+            >
+              {value}
+            </div>
+          </div>
+        )}
+      />
+      <Countdown
+        render={value => (
+          <div style={{ position: 'relative', display: 'inline-block' }}>
+            <CircleProgress reverse percentage={((10 - value) / 10) * 100} />
+            <div
+              style={{
+                position: 'absolute',
+                display: 'flex',
+                width: '100%',
+                height: '100%',
+                top: 0,
+                left: 0,
+                textAlign: 'center',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                fontSize: 40,
+                fontWeight: 'bold',
+              }}
+            >
+              {value}
+            </div>
+          </div>
+        )}
+      />
+    </Example>
+
     <NewPlayer />
+
+    <Example name="Button">
+      <CircleProgress />
+    </Example>
 
     <Example name="Button">
       <Button>Button</Button>
