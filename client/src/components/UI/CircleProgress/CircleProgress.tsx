597ee5735180bca9e10aa12ebf9a1d653d697065
Denys Izhetskyi <mail.bigjey@gmail.com>
2019-07-10 00:21:45 +0200

    added CircleProgress component

---
+++ client/src/components/UI/CircleProgress/CircleProgress.tsx
@@ -0,0 +1,45 @@
+import './CircleProgress.scss';
+
+import React from 'react';
+
+const CircleProgress = ({
+  radius = 100,
+  thikness = 15,
+  percentage = 20,
+  reverse = false,
+  animationSpeed = 100,
+}) => {
+  const stroke = radius - (radius - thikness);
+  const r = radius - thikness + stroke / 2;
+  const length = Math.PI * 2 * r;
+  let filled = (length * percentage) / 100;
+  let offset = 0;
+
+  if (reverse) {
+    filled = length - filled;
+    offset = -(length - filled);
+  }
+
+  return (
+    <>
+      <svg className="CircleProgress" width={radius * 2} height={radius * 2}>
+        <circle
+          className="CircleProgress--circle"
+          cx={radius}
+          cy={radius}
+          r={r}
+          fill="none"
+          strokeWidth={stroke}
+          strokeDasharray={`${filled} ${length}`}
+          strokeDashoffset={offset}
+          stroke="#000"
+          style={{
+            transition: `${animationSpeed}ms linear`,
+          }}
+        />
+      </svg>
+    </>
+  );
+};
+
+export { CircleProgress };
