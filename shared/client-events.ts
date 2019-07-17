feb39e62bd441ad5a114903f45ee28fdbe2b2a70
Denys Izhetskyi <mail.bigjey@gmail.com>
2019-07-17 02:06:39 +0200

    some refactoring

--- shared/client-events.ts
+++ shared/client-events.ts
@@ -3,3 +3,5 @@ export const JOIN_GAME: string = 'join-game';
 export const NEW_GAME: string = 'new-game';
 export const LEAVE_GAME: string = 'leave-game';
 export const TOGGLE_READY: string = 'toggle-ready';
+export const CONNECTION: string = 'connection';
+export const DISCONNECT: string = 'disconnect';
